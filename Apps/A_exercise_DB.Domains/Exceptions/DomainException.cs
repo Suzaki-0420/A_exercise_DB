c0d965988ccb4b87677e910b3c7329673714e4ca
H-Akaogi <hazumi.akaogi@wemex.com>
2026-07-06 16:49:12 +0900

    赤荻 Exceptions/DomainException 作成

---
+++ Apps/A_exercise_DB.Domains/Exceptions/DomainException.cs
@@ -0,0 +1,18 @@
+namespace RestAPI_Exercise.Application.Exceptions;
+/// <summary>
+/// 業務ルール違反を表す例外クラス
+/// </summary>
+public class DomainException : Exception
+{
+    /// <summary>
+    /// ドメイン例外
+    /// </summary>
+    /// <param name="message">例外メッセージ</param>
+    public DomainException(string message) : base(message) { }
+    /// <summary>
+    /// ドメイン例外
+    /// </summary>
+    /// <param name="message">例外メッセージ</param>
+    /// <param name="innerException"></param>
+    public DomainException(string message, Exception innerException) : base(message, innerException) { }
+}
