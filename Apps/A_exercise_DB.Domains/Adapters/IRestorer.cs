995eddfd69d3c942167b2713637e4bbb57a17da1
H-Akaogi <hazumi.akaogi@wemex.com>
2026-07-07 09:04:20 +0900

    赤荻 IRestorer 作成

---
+++ Apps/A_exercise_DB.Domains/Adapters/IRestorer.cs
@@ -0,0 +1,8 @@
+namespace A_Exercise_DB.Domains.Adapters;
+/// <summary>
+/// EntityをDomainObjectに変換するインターフェース
+/// </summary>
+public interface IRestorer<TDomain, TTarget>
+{
+    Task<TDomain> RestoreAsync(TTarget target);
+}
