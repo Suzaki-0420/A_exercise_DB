967246883ef6f60e95838823ca2aa276d9aa608c
H-Akaogi <hazumi.akaogi@wemex.com>
2026-07-07 09:03:28 +0900

    赤荻 IConverter 作成

---
+++ Apps/A_exercise_DB.Domains/Adapters/IConverter.cs
@@ -0,0 +1,8 @@
+namespace A_Exercise_DB.Domains.Adapters;
+/// <summary>
+/// DomainObjectをEntityに変換するアダプターのインターフェース
+/// </summary>
+public interface IConverter<TDomain, TTarget>
+{
+    Task<TTarget> ConvertAsync(TDomain domain);
+}
