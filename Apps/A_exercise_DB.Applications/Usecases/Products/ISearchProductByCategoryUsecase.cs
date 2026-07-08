57745912de211c55f3de9d7da95f6fb01e63ffbe
toukaon66 <tou.kaon.howsplan@gmail.com>
2026-07-08 13:48:09 +0900

    董　商品検索Usecase追加

---
+++ Apps/A_exercise_DB.Applications/Usecases/Products/ISearchProductByCategoryUsecase.cs
@@ -0,0 +1,14 @@
+using A_exercise_DB.Domains.Models;
+namespace A_exercise_DB.Applications.Usecases.Products;
+/// <summary>
+/// ユースケース:[商品をカテゴリー検索をする]を実装するインターフェイス
+/// </summary>
+public interface ISearchProductByCategoryUsecase
+{
+    /// <summary>
+    /// 指定されたカテゴリーの商品を返す
+    /// </summary>
+    /// <param name="productCategoryId"></param>
+    /// <returns>カテゴリー検索結果</returns>
+    Task<List<Product>> ExecuteAsync(int productCategoryId);
+}
---
+++ Apps/A_exercise_DB.Applications/Usecases/Products/SearchProductByCategoryUsecase.cs
@@ -0,0 +1,31 @@
+using A_exercise_DB.Domains.Models;
+using A_exercise_DB.Domains.Repositories;
+
+namespace A_exercise_DB.Applications.Usecases.Products;
+
+public class SearchProductByCategoryUsecase : ISearchProductByCategoryUsecase
+{
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="repository">商品CRUD操作リポジトリ</param>
+    private readonly IProductRepository _repository;
+
+    public SearchProductByCategoryUsecase(IProductRepository repository)
+    {
+        _repository = repository;
+    }
+
+    /// <summary>
+    /// 指定されたカテゴリーで商品を部分一致検索した結果を返す
+    /// </summary>
+    /// <param name="productCategoryId">商品カテゴリー</param>
+    /// <returns>カテゴリー検索結果</returns>
+    // <exception cref="NotFoundException">該当データが存在しない場合にスローされる</exception>
+    public async Task<List<Product>> ExecuteAsync(int productCategoryId)
+    {
+        var result = await _repository
+            .SelectByProductCategoryIdAsync(productCategoryId);
+        return result;
+    }
+}
