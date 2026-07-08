83a820393bdd81b857f5d6d83689868db37afa30
Suzaki0420 <chihoko.suzaki@wemex.com>
2026-07-08 11:33:14 +0900

    須崎 依存性注入ファイル追加

--- Apps/A_exercise_DB.Presentations/Configs/ApplicationDependencyExtensions.cs
+++ Apps/A_exercise_DB.Presentations/Configs/ApplicationDependencyExtensions.cs
@@ -6,8 +6,8 @@ using A_exercise_DB.Infrastructures.Repositories;
 using A_exercise_DB.Domains.Repositories;
 using A_exercise_DB.Domains.Models;
 using A_exercise_DB.Infrastructures.Shared;
-using A_exercise_DB.Application.Security;
-using A_exercise_DB.Infrastructures.Security;
+using A_exercise_DB.Applications.Usecases;
+
 
 namespace A_exercise_DB.Presentations.Configs;
 /// <summary>
@@ -49,31 +49,32 @@ public static class ApplicationDependencyExtensions
             options.UseNpgsql(connectstr);
         });
 
-        // ドメインオブジェクト:BookSctockとBookStockEntityの相互変換クラス
-        services.AddScoped<BookStockEntityAdapter>();
-        // ドメインオブジェクト:BookCategoryとBookCategoryEntityの相互変換クラス
-        services.AddScoped<BookCategoryEntityAdapter>();
-        // ドメインオブジェクト:BookとBookEntityの相互変換クラス
-        services.AddScoped<BookEntityAdapter>();
-        // 商品、商品カテゴリ、商品在庫オブジェクトの相互変換Factoryクラス
-        services.AddScoped<BookFactory>();
+        services.AddScoped<DepartmentEntityAdapter>();
+        services.AddScoped<EmployeeEntityAdapter>();
+        services.AddScoped<EmployeeAccountEntityAdapter>();
 
-        services.AddScoped<UserEntityAdapter>();
-        services.AddScoped<RoleEntityAdapter>();
+        services.AddScoped<CustomerEntityAdapter>();
+        services.AddScoped<OrderStatusEntityAdapter>();
+        services.AddScoped<PaymentMethodEntityAdapter>();
 
-        services.AddScoped<IRoleRepository, RoleRepository>();
+        services.AddScoped<OrdersEntityAdapter>();
+        services.AddScoped<OrdersDetailEntityAdapter>();
 
-        // ドメインオブジェクト:商品カテゴリのCRUD操作Repositoryインターフェイス
-        services.AddScoped<IBookCategoryRepository, BookCategoryRepository>();
-        // ドメインオブジェクト:商品カテゴリのCRUD操作Repositoryインターフェイス
-        services.AddScoped<IBookRepository, BookRepository>();
-        // Unit of Workパターンを利用したトランザクション制御インターフェイス
+        services.AddScoped<ProductCategoryEntityAdapter>();
+        services.AddScoped<ProductStockEntityAdapter>();
+        services.AddScoped<ProductEntityAdapter>();
+
+        // Product・ProductCategory・ProductStock復元用Factory
+        services.AddScoped<ProductFactory>();
+
+        services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+        services.AddScoped<IEmployeeAccountRepository, EmployeeAccountRepository>();
+
+        services.AddScoped<IProductCategoryRepository, ProductCategoryRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+
         services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-        services.AddScoped<IUserRepository, UserRepository>();
-
-        // JWTの発行・検証インターフェイスの実装
-        services.AddSingleton<IJwtTokenProvider, JwtTokenProvider>();
 
         return services;
     }
@@ -84,24 +85,8 @@ public static class ApplicationDependencyExtensions
     private static IServiceCollection AddApplicationLayerDependencies(
     this IServiceCollection services, IConfiguration config)
     {
-        services.AddScoped<IRegisterBookUsecase, RegisterBookUsecase>();
-        services.AddScoped<ISearchBookByKeywordUsecase, SearchBookByKeywordUsecase>();
-        services.AddScoped<IUpdateBookUsecase, UpdateBookUsecase>();
-        services.AddScoped<IGetCategoryUsecase, GetCategoryUsecase>();
-        services.AddScoped<IDeleteBookUsecase, DeleteBookUsecase>();
-        services.AddScoped<IRegisterUserUsecase, RegisterUserUsecase>();
-        services.AddScoped<IGetRoleUsecase, GetRoleUsecase>();
+        //services.AddScoped<IRegisterBookUsecase, RegisterBookUsecase>();
 
-        // ASP.NET Core Identityのパスワードハッシュ化・検証機能
-        services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
-        // PBKDF2アルゴリズムを利用したパスワードハッシュ化・検証機能
-        services.AddScoped<IPasswordHashingService, PBKDF2PasswordHashingService>();
-
-        // JwtSettingsをバインドしてDIに登録する
-        services.Configure<JwtSettings>(config.GetSection("JwtSettings"));
-
-        // ユースケース:[ログインする]を実現するインターフェイス
-        services.AddScoped<IAuthenticateUserUsecase, AuthenticateUserUsecase>();
         return services;
     }
 
@@ -115,12 +100,8 @@ public static class ApplicationDependencyExtensions
         services.AddControllers();
 
         // RegisterBookViewModelからドメインオブジェクト:Bookへ変換するアダプタ
-        services.AddScoped<RegisterBookViewModelAdapter>();
+        //services.AddScoped<RegisterBookViewModelAdapter>();
 
-        // UpdateBookViewModelからドメインオブジェクト:Bookへ変換するアダプタ
-        services.AddScoped<UpdateBookViewModelAdapter>();
-
-        services.AddScoped<RegisterUserViewModelAdapter>();
         return services;
     }
 
