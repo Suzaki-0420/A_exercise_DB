dd0cb05e866751f6c95101e669539399d209e3c2
toukaon66 <tou.kaon.howsplan@gmail.com>
2026-07-08 15:21:51 +0900

    董 SearchProductByCategoryController作成

---
+++ Apps/A_exercise_DB.Presentations/Controllers/SearchProductByCategoryController.cs
@@ -0,0 +1,38 @@
+using A_exercise_DB.Applications.Usecases.Products;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace A_exercise_DB.Presentations.Controllers; 
+/// <summary>
+/// ユースケース:[商品をカテゴリー検索をする]を実現するコントローラ
+/// </summary>
+[ApiController]
+[Route("admin/product")]
+// [SwaggerTag("商品をカテゴリー検索API")]
+public class SearchProductByCategory : ControllerBase
+{
+    private readonly ISearchProductByCategoryUsecase _usecase;
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="usecase">ユースケース:[商品をカテゴリー検索する]を実現するインターフェイス</param>
+    public SearchProductByCategory(ISearchProductByCategoryUsecase usecase)
+    {
+        _usecase = usecase;
+    }
+    /// <summary>
+    /// キーワードで商品を検索する
+    /// </summary>
+    /// <param name="productCategoryId">検索Id</param>
+    /// <returns>検索結果の商品一覧</returns>
+    
+    // [Authorize]
+    [HttpGet]
+    // [SwaggerResponse(StatusCodes.Status200OK, "検索に成功した場合、商品リストを返す", typeof(List<Product>))]
+    public async Task<IActionResult> Search(int productCategoryId)
+    {
+        // 商品キーワード検索する
+        var result = await _usecase.ExecuteAsync(productCategoryId);
+        return Ok(result);
+    }
+}
