e7b6539101f7e84369fbc177347ef0e7c79e255e
H-Akaogi <hazumi.akaogi@wemex.com>
2026-07-10 09:58:31 +0900

    赤荻 UpdateOrderStatus関連ViewModel 作成

---
+++ Apps/A_exercise_DB.Presentations/ViewModels/UpdateOrderStatusInputViewModel.cs
@@ -0,0 +1,45 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace A_exercise_DB.Presentations.ViewModels;
+
+/// <summary>
+/// 注文ステータス更新入力ViewModel
+/// </summary>
+public class UpdateOrderStatusInputViewModel
+{
+    /// <summary>
+    /// タイトル
+    /// </summary>
+    public string Title { get; set; } = "注文ステータス更新(入力)";
+
+    /// <summary>
+    /// 注文ID
+    /// </summary>
+    public Guid OrderId { get; set; }
+
+    /// <summary>
+    /// 購入日時
+    /// </summary>
+    public string OrderDate { get; set; } = string.Empty;
+
+    /// <summary>
+    /// 顧客アカウント名
+    /// </summary>
+    public string CustomerAccountName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// 注文内容
+    /// </summary>
+    public string OrderContent { get; set; } = string.Empty;
+
+    /// <summary>
+    /// 注文ステータス
+    /// </summary>
+    [Required(ErrorMessage = "注文ステータスは必須です。")]
+    public int OrderStatus { get; set; }
+
+    /// <summary>
+    /// 注文ステータス選択肢
+    /// </summary>
+    public List<OrderStatusOptionViewModel> OrderStatusList { get; set; } = new();
+}
