587ffcf1b194cf7609bb671d4b471d6c60db689e
H-Akaogi <hazumi.akaogi@wemex.com>
2026-07-16 13:44:25 +0900

    赤荻 本番環境でSwaggerを有効化

--- Apps/A_exercise_DB.Presentations/Program.cs
+++ Apps/A_exercise_DB.Presentations/Program.cs
@@ -3,26 +3,37 @@ using A_exercise_DB.Presentations.Configs;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddApplicationDependencies(builder.Configuration);
+builder.Services.AddApplicationDependencies(
+    builder.Configuration);
+
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var swaggerEnabled =
+    builder.Configuration.GetValue<bool>(
+        "Swagger:Enabled");
+
+// Development環境、または設定で有効化された場合にSwaggerを公開
+if (app.Environment.IsDevelopment()
+    || swaggerEnabled)
 {
     app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// 現在の本番環境はNginxもHTTP運用のため、
+// ProductionではHTTPSリダイレクトしない
+if (!app.Environment.IsProduction())
+{
+    app.UseHttpsRedirection();
+}
+
 app.UseAuthentication();
 app.UseAuthorization();
-app.UseStaticFiles();
 
 app.MapControllers();
 
